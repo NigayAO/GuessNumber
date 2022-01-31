46d83d7c60295b62198304ea7ab5009c6d8fca15
Alik <Nigay_AO@bk.ru>
2022-01-31 17:48:31 +0300

    Initial Commit

---
+++ GuessNumber/GuessNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberApp.swift
+//  GuessNumber
+//
+//  Created by Alik Nigay on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
